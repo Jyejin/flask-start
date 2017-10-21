8b9c9225c9aec5e5d24d6b4a65388e2fa6f22817
Ubuntu <ubuntu@ip-172-31-6-30.ap-northeast-2.compute.internal>
2017-10-21 09:45:51 +0000

    add bot function

--- Dockerfile
+++ Dockerfile
@@ -13,9 +13,12 @@ ENV LC_ALL ko_KR.UTF-8
 RUN apt-get install -y python3-pip python3-dev python3-virtualenv fabric \
       libpq-dev libjpeg-dev libxml2-dev libxslt-dev libfreetype6-dev libffi-dev \
       postgresql-client git curl wget
+      
 ADD . /flask-start
 WORKDIR /flask-start
+
 RUN pip3 install --upgrade pip
+RUN pip3 install requests
 RUN pip3 install -r requirements.txt
 ENTRYPOINT ["python3"]
 CMD ["hello.py"]
--- hello.py
+++ hello.py
@@ -7,6 +7,10 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 import testing
 
+# For Chatbot
+import json
+import requests
+
 app=Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY']='hard to guess string'
@@ -35,9 +39,40 @@ def extract():
         name=form.name.data
         result=testing.all_new(testing.split_stock_sentence(name))
         #table=db.extracting(name)
-        form.name.data=u''
+        form.name.data=''
     return render_template('extract.html',form=form,name=name,result=result)
+
+
+default = ["도와줘요홍길동"]
+
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+    userRequest = json.loads(request.get_data().decode('utf-8'))
+    print(userRequest)
+    result=None
+    if userRequest['content'] == '도와줘요홍길동':
+        return """{"message": {"text":"궁금하신 주식정보를 물어봐주세요"},"keyboard": {"type": "text"}}"""
+
+
+    elif userRequest['content'] == '엘지전자 최저가':
+        return """{"message": {"text":"삼성전자최저가는 삘릴리 입니다"},"keyboard": {"type": "text"}}"""
+
+	
+    else:
+        name=userRequest['content']
+        result=testing.all_new(testing.split_stock_sentence(name))
+        print(result)
+        return """{"message": {"text":"삼성전자최저가는 삘릴리 입니다"},"keyboard": {"type": "text"}}"""
+
+
+
+# 챗봇 Keyboard Initialize 
+@app.route('/keyboard', methods=['GET', 'POST'])
+def key():
+    return """{ "type" : "buttons", 
+                "buttons" : """+'["'+'","'.join(default)+'"]'+"""}"""
 
 
 if __name__ == "__main__":
     app.run(port=5000,host='0.0.0.0',debug=True)
+
