27edaa625ab77d4b463b18bcf85a8d0c2f1a4cf3
yejin <dbswjd1977@naver.com>
2017-12-21 13:42:55 +0900

    썸네일 함수 수정

--- drawing.py
+++ drawing.py
@@ -9,7 +9,11 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.finance import candlestick_ohlc
 from setting import Settings
+from datetime import date
+from flask import Blueprint
+from pandas import DataFrame
 
+thumbnail = Blueprint("thumbnail", __name__)
 settings = Settings()
 
 def pullStockData(name):
@@ -19,45 +23,46 @@ def pullStockData(name):
     df =settings.stocks[temp]
     return df
 
-def chart(stock_df):
-    #Reset the index to remove Date column from index
-    df_ohlc = stock_df.reset_index()
-
-    # Naming columns
-    # df_ohlc.columns = ["Date","Open","High",'Low',"Close"]
+def draw_candlechart(df_ohlc, fmt='%Y%m%d', freq=7, **kwargs):
+    df_ohlc = df_ohlc.reset_index()
     df_ohlc.columns = ["Date", "Adj Close", "Close" ,"High", "Low", "Open",  "Volume"]
+    df_ohlc = DataFrame(df_ohlc,columns=["Date", "Open","High", "Low", "Close","Volume"])
+    df_ohlc=df_ohlc[0:90]
 
-    #Converting dates column to float values
     df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+    ohlc_data_arr = np.array(df_ohlc)
+    ohlc_data_arr2 = np.hstack(
+        [np.arange(ohlc_data_arr[:,0].size)[:,np.newaxis], ohlc_data_arr[:,1:]])
+    ndays = ohlc_data_arr2[:,0]  # array([0, 1, 2, ... n-2, n-1, n])
 
-    #Making plot
-    fig = plt.figure()
-    ax1 = plt.subplot2grid((6,1), (0,0), rowspan=6, colspan=1)
-    #ax1.grid(True)
+    # Convert matplotlib date numbers to strings based on `fmt`
+    dates = mdates.num2date(ohlc_data_arr[:,0])
+    date_strings = []
+    for date in dates:
+        date_strings.append(date.strftime(fmt))
+    fig, ax = plt.subplots()
 
-    #Converts raw mdate numbers to dates
-    ax1.xaxis_date()
-    plt.xlabel("Date")
+    candlestick_ohlc(ax, ohlc_data_arr2, **kwargs, width=0.6, colorup='r', colordown='b')
+    #candlestick_ohlc(ax, df_ohlc.values, width=0.6, colorup='r', colordown='b')
+    # Format x axis
+    ax.set_xticks(ndays[::freq])
+    ax.set_xticklabels(date_strings[::freq], rotation=45, ha='right')
+    ax.set_xlim(ndays.min(), ndays.max())
 
-    #Making candlestick plot
-    candlestick_ohlc(ax1,df_ohlc.values,width=1, colorup='#2B64DF', colordown='#D5161B')
-    # 다음 값은 네이버 값입니다.
-    #2B64DF #D5161B
     plt.ylabel("Price")
-    ##아래 것이 무엇인지 잘 모르겠다.
-    plt.legend(['negative','positive'])
     plt.grid(True, alpha=0.75)
-    # 파일이 안 나올 때 아래 명령을 이용해서 확인한다.
+
     #plt.show()
 
     canvas=FigureCanvas(fig)
 
     fileName = "temp_candlestick.png"
-    #temp = "Temp_Folder/" + fileName
+    temp = "Temp_Folder/" + fileName
     FigureCanvas(fig).print_png(fileName, dpi=300)
 
     return fileName
 
+@thumbnail.route('/<certainName>')
 def making_chart(certainName, year='2017'):
     #이름이 들어가고, loc로 년도가 들어가야 한다.
 
@@ -65,6 +70,4 @@ def making_chart(certainName, year='2017'):
     stockChart = stockName.loc[year]
 
     #loc 한 걸 차트 함수로 받아줘야 한다.
-    return (chart(stockChart))
-
-#print(making_chart("삼성전자"))
+    return (draw_candlechart(stockChart))
